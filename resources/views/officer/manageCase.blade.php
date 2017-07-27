83a11ae4635996aa046d018cf81aa0e78affbeda
BOMBGIT <noppon.kumpdetch@gmail.com>
2017-07-27 11:24:43 +0700

    @extends footer home, managecase

--- resources/views/officer/manageCase.blade.php
+++ resources/views/officer/manageCase.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" class="route-index">
+<html lang="{{ config('app.locale') }}" class="route-index">
 
 <head>
 	<meta charset="utf-8">
@@ -203,28 +203,8 @@
 			</div>
 	</section>
 	<br>
-	<footer class="footer">
-		<div class="container">
-			<div class="columns">
-				<div class="column is-3">
-					<div id="about" class="content"> <strong xmlns:dct="#" href="#" property="dct:title" rel="dct:type">Crisis Response</strong> by <a xmlns:cc="#" href="#" property="cc:attributionName" rel="cc:attributionURL">Aidsrightsthailand</a>. </div>
-				</div>
-				<div class="column is-5">
-					<div id="share" class="content"> </div>
-				</div>
-				<div class="column is-4">
-					<div id="sister">
-						<p><small> <strong>ที่อยู่</strong> : </small>
-						</p>
-						<p><small>133/235 หมู่บ้านรื่นฤดี3 ถนนหทัยราษฎร์ แขวงมีนบุรี เขตมีนบุรี กทม 10510 โทรศัพท์ 02-171-5135-6 โทรสาร 02-1715124 </small>
-						</p>
-					</div>
-				</div>
-			</div>
-			<p id="tsp"> <small> Source code licensed <a href="#">HISO</a>. <br>
-      Website content licensed <a rel="license" href="http://www.hiso.or.th">www.hiso.or.th</a>. </small> </p>
-		</div>
-	</footer>
+	
+	@extends('footer')
 	
 </body>
 
