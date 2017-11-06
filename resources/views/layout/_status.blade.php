5a4f13117c7934726c60bd28d67bbca2ff319fcb
tonglar <glarexe@gmail.com>
2017-11-06 16:46:34 +0700

    edit status

--- resources/views/layout/_status.blade.php
+++ resources/views/layout/_status.blade.php
@@ -1,49 +1,79 @@
 
 <div class="timeline-item">
-    <div class="timeline-icon">
+
         @if($data->status > 1 )
+        <div class="timeline-icon">
         <img src="images/markx40.png" alt="">
+        </div>
+        <div class="timeline-content">
+            <h2>  </h2>
+            <p>
+                ระบบดำเนินการรับเรื่องเรียบร้อยแล้ว
+            </p>
+
+        </div>
         @else
+        <div class="timeline-icon">
         <img src="images/minusx40.png" alt="">
-        @endif
-    </div>
-    <div class="timeline-content">
-        <h2>  </h2>
-        <p>
-            ระบบดำเนินการรับเรื่องเรียบร้อยแล้ว
-        </p>
+            <div class="timeline-content">
+                <h2>  </h2>
+                <p>
+                    ยังไม่ได้รับเรื่อง
+                </p>
 
-    </div>
+            </div>
+        </div>
+
+    @endif
+
 </div>
 
 <div class="timeline-item">
-    <div class="timeline-icon">
         @if($data->status > 2 )
+            <div class="timeline-icon">
             <img src="images/markx40.png" alt="">
+                <div class="timeline-content right">
+                    <h2>  </h2>
+                    <p>
+                        เจ้าหน้าที่กำลังดำเนินการอยู่
+                    </p>
+                </div>
         @else
-            <img src="images/minusx40.png" alt="">
+              <div class="timeline-icon">
+              <img src="images/minusx40.png" alt="">
+              </div>
+                    <div class="timeline-content right">
+                        <h2>  </h2>
+                        <p>
+                            ยังไม่ได้ดำเนินการ
+                        </p>
+                    </div>
         @endif
-    </div>
-    <div class="timeline-content right">
-        <h2>  </h2>
-        <p>
-            เจ้าหน้าที่กำลังดำเนินการอยู่
-        </p>
-    </div>
+
+
 </div>
 
 <div class="timeline-item">
-    <div class="timeline-icon">
         @if($data->status > 3 )
-            <img src="images/markx40.png" alt="">
+        <div class="timeline-icon">
+        <img src="images/markx40.png" alt="">
+        </div>
+        <div class="timeline-content">
+            <h2> - </h2>
+            <p>
+                การดำเนินการเสร็จสิ้น
+            </p>
+        </div>
         @else
+        <div class="timeline-icon">
             <img src="images/minusx40.png" alt="">
-        @endif
-    </div>
-    <div class="timeline-content">
-        <h2> - </h2>
-        <p>
-            การดำเนินการเสร็จสิ้น
-        </p>
-    </div>
+        </div>
+        <div class="timeline-content">
+            <h2> - </h2>
+            <p>
+                ยังไม่เสร็จสิ้น
+            </p>
+        </div>
+    @endif
+
 </div>
