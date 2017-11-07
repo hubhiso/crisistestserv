aafa8c2acf6c6a532566bbcb705a61219aaaf1c9
BOMBGIT <noppon.kumpdetch@gmail.com>
2017-11-07 10:34:54 +0700

    - update part css font awesome

--- resources/views/input.blade.php
+++ resources/views/input.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="{{ asset('bulma/css/bulma.css') }}" rel="stylesheet">
-    <link href="{{ asset('font-awesome/css/font-awesome.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/font-awesome/css/font-awesome.css') }}" rel="stylesheet">
     {{ Html::script('js/jquery.min.js') }}
     <title> CRS </title>
 
