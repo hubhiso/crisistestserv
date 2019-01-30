4f8885ef2ee0c9dfe71ef0a004098b8912868ec0
Noppon Kumpdetch <>
2019-01-30 11:50:13 +0700

    update footer

--- resources/views/footer.php
+++ resources/views/footer.php
@@ -2,7 +2,8 @@
 		<div class="container">
 			<div class="columns">
 				<div class="column is-3">
-					<div id="about" class="content"> <strong >Crisis Response</strong> by <a  href="http://www.aidsrightsthailand.com" >Aidsrightsthailand</a>. </div>
+					<div id="about" class="content"> <strong >Crisis Response System</strong>  </div>
+					<!--a class="button is-outlined" href="{{ 'my-page' }}">หน่วยงานสนับสนุน</a-->
 					<a class="button is-outlined" href="support">หน่วยงานสนับสนุน</a>
 
 				</div>
@@ -10,15 +11,10 @@
 					<div id="share" class="content"> </div>
 				</div>
 				<div class="column is-4">
-					<div id="sister">
-						<p><small> <strong>ที่อยู่</strong> : </small>
-						</p>
-						<p><small>133/235 หมู่บ้านรื่นฤดี3 ถนนหทัยราษฎร์ แขวงมีนบุรี เขตมีนบุรี กทม 10510 โทรศัพท์ 02-171-5135-6 โทรสาร 02-1715124 </small>
-						</p>
+					<div id="right">
 					</div>
 				</div>
 			</div>
-			<p id="tsp"> <small> Source code licensed <a href="http://www.hiso.or.th">HISO</a>. <br>
-                Website content licensed <a rel="license" href="http://www.hiso.or.th">www.hiso.or.th</a>. </small> </p>
+			<p id="tsp"> <small> Source code licensed <a href="http://www.hiso.or.th">HISO</a>.  </small> </p>
 		</div>
 </footer>
