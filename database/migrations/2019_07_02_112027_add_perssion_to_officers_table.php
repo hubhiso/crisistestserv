ab78bbfeb495914eb26d34327dd870ec9dee9ae3
Noppon Kumpdetch <>
2019-07-02 14:22:09 +0700

    add field permission

---
+++ database/migrations/2019_07_02_112027_add_perssion_to_officers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPerssionToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('officers', function (Blueprint $table) {
+            $table->enum('p_view_all', ['yes', 'no'])->nullable();
+            $table->enum('p_receive', ['yes', 'no'])->nullable();
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('officers', function (Blueprint $table) {
+            //
+        });
+    }
+}
