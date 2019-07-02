3c233e8ea70aa3fddcb759b5d15c098a18611ce3
Noppon Kumpdetch <>
2019-07-02 14:42:58 +0700

    update migrate

--- database/migrations/2019_07_02_112027_add_perssion_to_officers_table.php
+++ database/migrations/2019_07_02_112027_add_perssion_to_officers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddPerssionToUsersTable extends Migration
+class AddPerssionToOfficersTable extends Migration
 {
     /**
      * Run the migrations.
