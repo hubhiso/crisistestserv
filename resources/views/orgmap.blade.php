060006bb5d34b0c021fbf6f92193cf6a489feac0
Noppon Kumpdetch <>
2020-04-14 23:18:51 +0700

    update css

--- resources/views/orgmap.blade.php
+++ resources/views/orgmap.blade.php
@@ -3,59 +3,69 @@
 
 <head>
 
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="">
-	<title>CRS</title>
-	<link rel="shortcut icon" href="../public/images/favicon.ico">
-	<link href="{{ asset('bulma/css/bulma.css') }}" rel="stylesheet">
+    <meta charset="utf-8">
+    <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <title>CRS</title>
+    <link href="{{ asset('bulma-0.8.0/css/bulma.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/mystyles.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Mitr:wght@200;300&display=swap" rel="stylesheet">
+
     <script src="js/jquery.min.js"></script>
-    <link href="{{ asset('css/font-awesome5.0.6/css/fontawesome-all.css') }}"
-		  rel="stylesheet">
+    <link href="{{ asset('css/font-awesome5.0.6/css/fontawesome-all.css') }}" rel="stylesheet">
 
-	<meta name="theme-color" content="#cc99cc"/>
-    
+    <meta name="theme-color" content="#cc99cc" />
 
-	<style>
-		.hideextra { white-space: nowrap; overflow: hidden; text-overflow:ellipsis; }
-	</style>
+
+    <style>
+    .hideextra {
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
+    </style>
 
 </head>
 
 
-<body class="layout-default">
+<body class="layout-default has-background-light">
 
-		@component('component.input_head') @endcomponent
+    @component('component.input_head') @endcomponent
 
-	<div class="container">
+    <div class="container">
 
-		<section class="section">
+        <section class="section">
 
-			<nav class="breadcrumb">
-				<ul>
-					<li><a href="{{ 'index.php' }}"><span class="icon is-small"><i class="fa fa-home"></i></span><span> หน้าหลัก </span></a>
-					</li>
-					<li class="is-active"><a><span class="icon is-small"><i class="fas fa-thumbtack"></i></span><span> พิกัดหน่วยงาน </span></a>
-					</li>
-				</ul>
-			</nav>
-			<iframe src="../PHP/crisisclusterorgmap/cluster.php" height="100%" width="100%" style="height:700px;"></iframe>	
-		</section>
-	</div>
+            <nav class="breadcrumb">
+                <ul>
+                    <li><a href="{{ 'index.php' }}"><span class="icon is-small"><i class="fa fa-home"></i></span><span>
+                                หน้าหลัก </span></a>
+                    </li>
+                    <li class="is-active"><a><span class="icon is-small"><i class="fas fa-thumbtack"></i></span><span>
+                                พิกัดหน่วยงาน </span></a>
+                    </li>
+                </ul>
+            </nav>
+            <iframe src="../PHP/crisisclusterorgmap/cluster.php" height="100%" width="100%"
+                style="height:700px;"></iframe>
+        </section>
+    </div>
 
 </body>
 
 
 
-<footer class="footer "style="background-color: #EEE;">
-  <div class="container  ">
-    <div class="content has-text-centered  ">
-      <p>Crisis Response System (CRS)
-	  </p>
-	  <p id="tsp"> <small> Source code licensed <a href="http://www.hiso.or.th">HISO</a>.  </small> </p>
+<footer class="footer " style="background-color: #EEE;">
+    <div class="container  ">
+        <div class="content has-text-centered  ">
+            <p>Crisis Response System (CRS)
+            </p>
+            <p id="tsp"> <small> Source code licensed <a href="http://www.hiso.or.th">HISO</a>. </small> </p>
+        </div>
     </div>
-  </div>
 </footer>
 
 </html>
--- resources/views/support.blade.php
+++ resources/views/support.blade.php
@@ -2,46 +2,53 @@
 <html lang="en">
 
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link href="{{ asset('bulma/css/bulma.css') }}" rel="stylesheet">
-	<link href="{{ asset('css/font-awesome5.0.6/css/fontawesome-all.css') }}"
-		  rel="stylesheet"> {{ Html::script('js/jquery.min.js') }}
+    <meta charset="utf-8">
+    <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}">
 
-	<title> CRS - Agency Support </title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="{{ asset('bulma-0.8.0/css/bulma.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/mystyles.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Mitr:wght@200;300&display=swap" rel="stylesheet">
+
+    <link href="{{ asset('css/font-awesome5.0.6/css/fontawesome-all.css') }}" rel="stylesheet">
+    {{ Html::script('js/jquery.min.js') }}
+
+    <title> CRS - Agency Support </title>
 
 
-	<link rel="stylesheet" type="text/css" href="css/uploadicon/new3.css" />
+    <link rel="stylesheet" type="text/css" href="css/uploadicon/new3.css" />
 
-	
+
 </head>
 
-<body>
+<body class=" has-background-light">
     <section class="hero is-primary wall3">
-		<div class="hero-body">
-			<div class="container">
-				<div class="columns is-vcentered">
-					<div class="column">
-						<p class="title"> Crisis Response System (CRS) </p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
+        <div class="hero-body">
+            <div class="container">
+                <div class="columns is-vcentered">
+                    <div class="column">
+                        <p class="title"> Crisis Response System (CRS) </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
 
     <section class="section">
         <div class="container has-text-centered">
 
-        <nav class="breadcrumb">
-            <ul>
-                <li><a href="{{ 'index.php' }}"><span class="icon is-small"><i class="fa fa-home"></i></span><span> หน้าหลัก </span></a>
-                </li>
-                <li class="is-active"><a><span class="icon is-small"><i class="fa fa-handshake"></i></span><span> ภาคีเครือข่าย </span></a>
-                </li>
-            </ul>
-        </nav>
+            <nav class="breadcrumb">
+                <ul>
+                    <li><a href="{{ 'index.php' }}"><span class="icon is-small"><i class="fa fa-home"></i></span><span>
+                                หน้าหลัก </span></a>
+                    </li>
+                    <li class="is-active"><a><span class="icon is-small"><i class="fa fa-handshake"></i></span><span>
+                                ภาคีเครือข่าย </span></a>
+                    </li>
+                </ul>
+            </nav>
 
             <h2 class="title"> ภาคีเครือข่าย </h2>
 
@@ -55,110 +62,110 @@
                                 <img src="images/logo/logo11-x100.png">
                             </figure>
                             <p class="is-size-6">สายด่วน 1442<br>กรมควบคุมโรค</p>
-                            </a>
+                        </a>
                         </a>
                     </article>
                 </div>
                 <div class="tile is-parent">
-                    <article class="tile is-child box"  >
-                    <a href="http://aidssti.ddc.moph.go.th">
-                        <figure class="image"  >
-                            <img src="images/logo/logo1-x100.png"  >
-                        </figure>
-                        <p class="is-size-6"  >สำนักโรคเอดส์ วัณโรค และโรคติดต่อทางเพศสัมพันธ์</p>
+                    <article class="tile is-child box">
+                        <a href="http://aidssti.ddc.moph.go.th">
+                            <figure class="image">
+                                <img src="images/logo/logo1-x100.png">
+                            </figure>
+                            <p class="is-size-6">สำนักโรคเอดส์ วัณโรค และโรคติดต่อทางเพศสัมพันธ์</p>
                         </a>
                     </article>
                 </div>
                 <div class="tile is-parent">
-                <article class="tile is-child box">
-                <a href="http://www.aidsrightsthailand.com">
-                        <figure class="image">
-                            <img src="images/logo/logo2-x100.png">
-                        </figure>
-                        <p class="is-size-6">มูลนิธิศูนย์คุ้มครองสิทธิ์ด้านเอดส์</p>
+                    <article class="tile is-child box">
+                        <a href="http://www.aidsrightsthailand.com">
+                            <figure class="image">
+                                <img src="images/logo/logo2-x100.png">
+                            </figure>
+                            <p class="is-size-6">มูลนิธิศูนย์คุ้มครองสิทธิ์ด้านเอดส์</p>
                         </a>
                     </article>
                 </div>
                 <div class="tile is-parent">
-                <article class="tile is-child box">
-                <a href="http://www.rlpd.go.th">
-                        <figure class="image">
-                            <img src="images/logo/logo3-x100.png">
-                        </figure>
-                        <p class="is-size-6">กรมคุ้มครองสิทธิและเสรีภาพ</p>
+                    <article class="tile is-child box">
+                        <a href="http://www.rlpd.go.th">
+                            <figure class="image">
+                                <img src="images/logo/logo3-x100.png">
+                            </figure>
+                            <p class="is-size-6">กรมคุ้มครองสิทธิและเสรีภาพ</p>
                         </a>
                     </article>
                 </div>
                 <div class="tile is-parent">
-                <article class="tile is-child box">
-                <a href="http://www.nhrc.or.th">
-                        <figure class="image">
-                            <img src="images/logo/logo4-x100.png">
-                        </figure>
-                        <p class="is-size-6">สำนักงานคณะกรรมการสิทธิมนุษยชนแห่งชาติ</p>
+                    <article class="tile is-child box">
+                        <a href="http://www.nhrc.or.th">
+                            <figure class="image">
+                                <img src="images/logo/logo4-x100.png">
+                            </figure>
+                            <p class="is-size-6">สำนักงานคณะกรรมการสิทธิมนุษยชนแห่งชาติ</p>
                         </a>
                     </article>
                 </div>
-                
+
             </div>
 
             <div class="tile is-ancestor">
                 <div class="tile is-parent">
-                <article class="tile is-child box">
-                <a href="http://www.1300thailand.m-society.go.th">
-                        <figure class="image">
-                            <img src="images/logo/logo5-x100.png">
-                        </figure>
-                        <p class="is-size-6">ศูนย์ช่วยเหลือสังคม</p>
+                    <article class="tile is-child box">
+                        <a href="http://www.1300thailand.m-society.go.th">
+                            <figure class="image">
+                                <img src="images/logo/logo5-x100.png">
+                            </figure>
+                            <p class="is-size-6">ศูนย์ช่วยเหลือสังคม</p>
                         </a>
                     </article>
                 </div>
                 <div class="tile is-parent">
-                <article class="tile is-child box">
-                <a href="http://www.lawyerscouncil.or.th">
-                        <figure class="image">
-                            <img src="images/logo/logo6-x100.png">
-                        </figure>
-                        <p class="is-size-6">สภาทนายความ ในพระบรมราชูปถัมภ์</p>
+                    <article class="tile is-child box">
+                        <a href="http://www.lawyerscouncil.or.th">
+                            <figure class="image">
+                                <img src="images/logo/logo6-x100.png">
+                            </figure>
+                            <p class="is-size-6">สภาทนายความ ในพระบรมราชูปถัมภ์</p>
                         </a>
                     </article>
                 </div>
                 <div class="tile is-parent">
-                <article class="tile is-child box">
-                <a href="http://www.aidsaccess.com">
-                        <figure class="image">
-                            <img src="images/logo/logo7-x100.png">
-                        </figure>
-                        <p class="is-size-6">สายด่วน 1663<br>มูลนิธิเข้าถึงเอดส์</p>
+                    <article class="tile is-child box">
+                        <a href="http://www.aidsaccess.com">
+                            <figure class="image">
+                                <img src="images/logo/logo7-x100.png">
+                            </figure>
+                            <p class="is-size-6">สายด่วน 1663<br>มูลนิธิเข้าถึงเอดส์</p>
                         </a>
                     </article>
                 </div>
                 <div class="tile is-parent">
-                <article class="tile is-child box">
-                <a href="http://www.thaiplus.net">
-                        <figure class="image">
-                            <img src="images/logo/logo8-x100.png">
-                        </figure>
-                        <p class="is-size-6">เครือข่ายผู้ติดเชื้อ<br>เอชไอวี/เอดส์ประเทศไทย</p>
+                    <article class="tile is-child box">
+                        <a href="http://www.thaiplus.net">
+                            <figure class="image">
+                                <img src="images/logo/logo8-x100.png">
+                            </figure>
+                            <p class="is-size-6">เครือข่ายผู้ติดเชื้อ<br>เอชไอวี/เอดส์ประเทศไทย</p>
                         </a>
                     </article>
                 </div>
-                
+
                 <div class="tile is-parent">
-                    <article class="tile is-child box" >
+                    <article class="tile is-child box">
                         <a href="http://www.hiso.or.th">
                             <figure class="image">
-                                <img  src="images/logo/logo10-x100.png">
+                                <img src="images/logo/logo10-x100.png">
                             </figure>
                             <p class="is-size-6">สำนักงานพัฒนาระบบ<br>ข้อมูลข่าวสารสุขภาพ</p>
                         </a>
                     </article>
                 </div>
                 <div class="tile is-parent">
-                    <article class="tile is-child box" >
+                    <article class="tile is-child box">
                         <a href="http://www.ago.go.th">
                             <figure class="image">
-                                <img  src="images/logo/logo12-x100.png">
+                                <img src="images/logo/logo12-x100.png">
                             </figure>
                             <p class="is-size-6">สำนักงานอัยการสูงสุด</p>
                         </a>
@@ -168,10 +175,11 @@
         </div>
     </section>
 
-    
+
 
     @extends('footer')
 
-        <script src="../js/bulma.js"></script>
+    <script src="../js/bulma.js"></script>
 </body>
+
 </html>
