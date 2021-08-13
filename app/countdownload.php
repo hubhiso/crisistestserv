1d84ada94d005629cc8d288ad1aad28826e7c8e8
Noppon Kumpdetch <>
2021-08-13 21:28:04 +0700

    + migrate countdownload + model countdownload

---
+++ app/countdownload.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class countdownload extends Model
+{
+    //
+    protected  $table = 'countdownload'; 
+    protected  $guarded = [];
+    public $timestamps = false;
+
+}
