a7be7e4b8b1354c2dc405fe4da826d120f944ee6
Noppon Kumpdetch <>
2021-08-25 11:51:10 +0700

    update page view

--- resources/views/officer/view_only.blade.php
+++ resources/views/officer/view_only.blade.php
@@ -5,11 +5,15 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title> CRS </title>
-	<link href="{{ asset('bulma/css/bulma.css') }}" rel="stylesheet">
+
+	<link href="{{ asset('bulma-0.8.0/css/bulma.css') }}" rel="stylesheet">
+	<link href="{{ asset('css/mystyles.css') }}" rel="stylesheet">
+
 	<link href="{{ asset('css/step.css') }}" rel="stylesheet">
 	<link href="{{ asset('css/font-awesome5.0.6/css/fontawesome-all.css') }}" rel="stylesheet"> 
 	{{ Html::script('js/jquery.min.js') }}
+
+    <title> ปกป้อง (CRS) </title>
 	<meta name="theme-color" content="#cc99cc"/>
 
 </head>
@@ -18,9 +22,9 @@
 @endphp
 <body class="layout-default">
 	<section class="hero is-medium ">
-		<div class="hero-head">
-			<div class="container">
-				@component('component.login_bar2') 
+		<div class="hero-head ">
+			<div class=" ">
+				@component('component.login_bar2')
 				@endcomponent
 			</div>
 		</div>
@@ -29,11 +33,31 @@
 		<form class="form-horizontal" role="form" method="POST" action="{{ route('officer.accept_c') }}">
 			<input type="hidden" name="_token" value="{{ csrf_token() }}">
 			<div class="container">
-				<nav class="breadcrumb">
+				<nav class="breadcrumb" aria-label="breadcrumbs">
 					<ul>
-						<li><a href="{{ route('officer.show',['mode_id' => " 0 "]) }}"><span class="icon is-small"><i class="fa fa-home"></i></span><span> หน้าหลัก </span></a>
+						<li>
+							<a href="{{ route('guest_home') }}">
+								<span class="icon is-small">
+									<i class="fas fa-home" aria-hidden="true"></i>
+								</span>
+								<span>หน้าหลัก</span>
+							</a>
 						</li>
-						<li class="is-active"><a><span class="icon is-small"><i class="fa fa-id-card"></i></span><span> ข้อมูลเบื้องต้น </span></a>
+						<li>
+							<a href="{{ route('officer.main') }}">
+								<span class="icon is-small">
+									<i class="fas fa-lock" aria-hidden="true"></i>
+								</span>
+								<span>ส่วนเจ้าหน้าที่</span>
+							</a>
+						</li>
+						<li class="is-active">
+							<a href="#">
+								<span class="icon is-small">
+									<i class="fa fa-id-card" aria-hidden="true"></i>
+								</span>
+								<span>ข้อมูลเบื้องต้น</span>
+							</a>
 						</li>
 					</ul>
 				</nav>
@@ -397,9 +421,13 @@
 
 	</from>
 
+	<br> 
+	@extends('officer.footer_m')
 
-	<br> @extends('footer')
 </body>
+
+<script src="{{ asset('bulma/clipboard-1.7.1.min.js') }}"></script>
+<script src="{{ asset('bulma/main.js') }}"></script>
 
 <script>
 	var p_id = $('#p_id').val();
@@ -407,27 +435,21 @@
     var p_ar = $('#p_area').val();
 
     var status_url = "{{route('officer.load_status',['prov_id' => ':p_id']) }}";
-    status_url = status_url.replace(':p_id', p_id+' '+p_po+' '+p_ar);
+    status_url = status_url.replace(':p_id', p_id + ' ' + p_po + ' ' + p_ar);
     console.log(status_url);
     $.ajax({
         type: 'GET',
         url: status_url,
-        success: function( data ) {
+        success: function(data) {
             //console.log(data);
             $('#i-receive').text(data.NotAcp);
             $('#i-additional').text(data.NotKeyIn);
             $('#i-process').text(data.NotOp);
         }
     });
-
-	if($show_data->file1==''){
-		console.log("file1 null");
-	}else{
-		console.log("file1 not null");
-	}
+	
 </script>
 
-<script src="{{ asset('bulma/clipboard-1.7.1.min.js') }}"></script>
-<script src="{{ asset('bulma/main.js') }}"></script>
+
 
 </html>
