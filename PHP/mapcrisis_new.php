847e0d536e5cec559339b550d8b64f6eaed3871c
Noppon Kumpdetch <>
2021-10-26 21:44:42 +0700

    add report map new

---
+++ PHP/mapcrisis_new.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="en" class="route-index">
+
+<head>
+
+    <meta http-equiv=”content-type” content=”text/html; charset=UTF-8″ />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv=”content-language” content=”th” />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name=”description” content=”ปกป้อง ระบบผู้ช่วยออนไลน์ที่บริการรับเรื่องร้องเรียนและคุ้มครองการละเมิดสิทธิ” />
+    <meta name=”keywords” content=”ปกป้อง,ปกป้อง thai” />
+
+    <meta name="theme-color" content="#d45c9c" />
+
+    <link rel="shortcut icon" href="../public/favicon.ico">
+
+    <link href="https://fonts.googleapis.com/css2?family=Mitr:wght@200;300&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" crossorigin="anonymous">
+
+    <link media="all" type="text/css" rel="stylesheet"
+        href="../public/bootstrap-datepicker/css/bootstrap-datepicker.min.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <link href="report.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="crisiscluster/leaflet_cluster.css" />
+    <link rel="stylesheet" href="crisiscluster/MarkerCluster.css" />
+    <link rel="stylesheet" href="crisiscluster/leaflet.css" />
+    <script src="crisiscluster/leaflet.js"></script>
+    <link rel="stylesheet" href="crisiscluster/MarkerCluster.css" />
+    <script src="crisiscluster/leaflet.markercluster.js"></script>
+
+    <title> ปกป้อง (CRS) </title>
+
+</head>
+
+<body class="bg-light">
+
+    <nav class="navbar navbar-light bg-white">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="../public/">
+                <img src="../public/images/favicon.ico" alt="" height="30">
+                <span class="text-secondary">ปกป้อง (CRS)</span>
+            </a>
+        </div>
+    </nav>
+
+    <div class="container-fluid p-4">
+
+        <nav aria-label="breadcrumb ">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item">
+                    <a href="../public"><span class="icon is-small">
+                            <i class="fas fa-home" aria-hidden="true"></i>
+                        </span>หน้าหลัก</a>
+                </li>
+                <li class="breadcrumb-item">
+                    <a href="../public/officer"><span class="icon is-small">
+                            <i class="fas fa-lock" aria-hidden="true"></i>
+                        </span>ส่วนเจ้าหน้าที่</a>
+                </li>
+                <li class="breadcrumb-item active" aria-current="page">
+                    <span class="icon is-small"><i class="fas fa-chart-bar" aria-hidden="true"></i></span>&nbsp;รายงาน
+                </li>
+            </ol>
+        </nav>
+
+        <div class="text-center ">
+
+            <div class="btn-group flex-wrap">
+                <a type="button" class="btn btn-white btn-rounded   align-items-stretch d-flex border"
+                    href="dashboard3_new.php">
+                    <div class=" icon-left d-flex align-items-center justify-content-center h4">
+                        <i class="fas fa-chart-bar" aria-hidden="true"></i>
+                    </div>
+                    &nbsp;&nbsp;&nbsp;
+                    <div class="text text-right ">
+                        <h6>Dashboard</h6>
+                        <span>สรุปสถานการณ์</span>
+                    </div>
+                </a>
+
+                <a type="button" class="btn btn-primary  btn-rounded   align-items-stretch d-flex border"
+                    href="mapcrisis_new.php">
+                    <div class=" icon-left d-flex align-items-center justify-content-center h4">
+                        <i class="far fa-map" aria-hidden="true"></i>
+                    </div>
+                    &nbsp;&nbsp;&nbsp;
+                    <div class="text text-right ">
+                        <h6>พิกัดการ</h6>
+                        <span>ละเมิดสิทธิ์</span>
+                    </div>
+                </a>
+
+                <a type="button" class="btn btn-white btn-rounded   align-items-stretch d-flex border"
+                    href="table.blade.php">
+                    <div class=" icon-left d-flex align-items-center justify-content-center h4">
+                        <i class="far fa-file-alt" aria-hidden="true"></i>
+                    </div>
+                    &nbsp;&nbsp;&nbsp;
+                    <div class="text text-right">
+                        <h6>สรุปข้อมูล</h6>
+                        <span>ภาพรวม</span>
+                    </div>
+                </a>
+
+                <a type="button" class="btn btn-white btn-rounded   align-items-stretch d-flex border" href="report_c1_new.php">
+                    <div class=" icon-left d-flex align-items-center justify-content-center h4">
+                        <i class="far fa-file-alt" aria-hidden="true"></i>
+                    </div>
+                    &nbsp;&nbsp;&nbsp;
+                    <div class="text text-right">
+                        <h6>สรุปกรณี</h6>
+                        <span>ละเมิดสิทธิ์</span>
+                    </div>
+                </a>
+
+
+                <a type="button" class="btn btn-white btn-rounded   align-items-stretch d-flex border"
+                    href="report_c2_new.php">
+                    <div class=" icon-left d-flex align-items-center justify-content-center h4">
+                        <i class="far fa-file-alt" aria-hidden="true"></i>
+                    </div>
+                    &nbsp;&nbsp;&nbsp;
+                    <div class="text text-right">
+                        <h6>ตารางสรุป</h6>
+                        <span>การละเมิดสิทธิ์</span>
+                    </div>
+                </a>
+
+                <a type="button" class="btn btn-white btn-rounded   align-items-stretch d-flex border"
+                    href="report_perfomance_new.php">
+                    <div class=" icon-left d-flex align-items-center justify-content-center h4">
+                        <i class="far fa-file-alt" aria-hidden="true"></i>
+                    </div>
+                    &nbsp;&nbsp;&nbsp;
+                    <div class="text text-right">
+                        <h6>ระยะเวลา</h6>
+                        <span>ดำเนินการ</span>
+                    </div>
+                </a>
+
+            </div>
+
+            <div class="p-3">
+                <p class="h3">พิกัดการละเมิดสิทธิ์</p>
+                <p class="h5">คลิกที่ตัวเลขบนแผนที่เพื่อขยายแผนที่ และแสดงพิกัดในบริเวณดังกล่าว</p>
+            </div>
+        </div>
+
+        <p class="subtitle ">
+            <strong> ข้อมูล ณ วันที่ (ด/ว/ป) </strong>
+
+            <?php echo "  : ",date("m/d/Y")," เวลา : ",date("h:i:sa"); ?>
+        </p>
+
+        <div name="map" class="p-3 chart-rounded">
+
+            <iframe src="crisiscluster/cluster.php" class="chart-rounded" height="100%" width="100%"
+                style="height:700px;"></iframe>
+        </div>
+
+    </div>
+
+    <!-- Footer -->
+    <footer class="text-center text-lg-start bg-light text-muted">
+
+        <!-- Copyright -->
+        <div class="text-center p-5" style="background-color: #dddddd;">
+            Crisis Response System (CRS)
+            <p id="tsp"> <small> Source code licensed <a href="http://www.hiso.or.th">HISO</a>. </small> </p>
+        </div>
+        <!-- Copyright -->
+    </footer>
+
+    <script src="../public/js/jquery.min.js"></script>
+    <script src="../public/bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
