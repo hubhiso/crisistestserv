939e2e92d87328a3904338d8a3c655e628a8a082
Noppon Kumpdetch <>
2021-10-30 15:43:53 +0700

    update 1

--- PHP/phpsql_dbinfo.php
+++ PHP/phpsql_dbinfo.php
@@ -5,15 +5,15 @@ $password="hiso";
 $database="crisistest";
 $hostname = "localhost";
 
-$connection=mysqli_connect ($hostname, $username, $password);
+$connection=mysql_connect ($hostname, $username, $password);
 
-mysqli_query("SET NAMES UTF8",$connection); 
+mysql_query("SET NAMES UTF8",$connection); 
 if (!$connection) {
 
   die('Not connected : ');
 }
 
-$db_selected = mysqli_select_db($connection, "crisisserv2");
+$db_selected = mysql_select_db($connection, "crisisserv2");
 if (!$db_selected) {
 
   die ('Can\'t use db : ');
